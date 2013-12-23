87f9992445c5a69e644398a8888a07dab3d84649
Stas Kobzar <stas@modulis.ca>
2013-12-23 01:00:47 -0500

    Add notify generate message function

--- res_zonkey_mwi.c
+++ res_zonkey_mwi.c
@@ -36,6 +36,9 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: $")
 #include "asterisk/config.h"
 #include "asterisk/cli.h"
 #include "asterisk/event.h"
+#include "asterisk/strings.h"
+#include "asterisk/netsock2.h"
+#include "asterisk/utils.h"
 
 #ifndef AST_MODULE
 #define AST_MODULE        "res_zonkey_mwi"
@@ -47,6 +50,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: $")
 #define LEN_MWI_TOTAG     256
 #define LEN_MWI_FROMTAG   256
 #define LEN_MWI_CALLID    256
+#define LEN_MWI_SIPMSG    4096 // same length chan_sip.c uses
 
 unsigned int realtime_enabled= 0;       // flag status of realtime configuration detected
 struct ast_event_sub *mwi_sub = NULL;   // Subscribe to MWI event
@@ -71,11 +75,15 @@ struct subscription {
 static void zonkey_mwi_cb(const struct ast_event *ast_event, void *data);
 static char *handle_cli_zonkeymwi_status(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a);
 static char *handle_cli_zonkeymwi_show_subscription(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a);
+static char *handle_cli_zonkeymwi_notify(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a);
 static struct subscription *find_watcher(char *name, char *domain);
+int send_notify(struct subscription *watcher, int msgnew, int msgold, struct ast_cli_args *a);
+void notify_create(struct ast_str *msg, struct subscription *watcher, int new, int old);
 
 static struct ast_cli_entry cli_zonkeymwi[] = {
   AST_CLI_DEFINE(handle_cli_zonkeymwi_status,             "Show Zonkey MWI status"),
-  AST_CLI_DEFINE(handle_cli_zonkeymwi_show_subscription,  "Show MWI subscription for user in domain")
+  AST_CLI_DEFINE(handle_cli_zonkeymwi_show_subscription,  "Show MWI subscription for user in domain"),
+  AST_CLI_DEFINE(handle_cli_zonkeymwi_notify,  "Send NOTIFY MWI to user in domain with defined messages number")
 };
 
 /*!
@@ -144,8 +152,8 @@ static void zonkey_mwi_cb(const struct ast_event *ast_event, void *data)
 static char *handle_cli_zonkeymwi_show_subscription(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a)
 {
   struct subscription *watcher;
-  char user[32];
-  char domain[256];
+  char user[LEN_MWI_USER];
+  char domain[LEN_MWI_DOMAIN];
 
   switch (cmd) {
   case CLI_INIT:
@@ -211,6 +219,127 @@ static char *handle_cli_zonkeymwi_status(struct ast_cli_entry *e, int cmd, struc
   }
 
   return CLI_SUCCESS;
+}
+
+/*!
+ * \brief Send NOTIFY MWI event to given user@domain with given number of messages.
+ * \param ast_cli_entry
+ * \param command
+ * \param command arguments
+ * \return char
+ */
+static char *handle_cli_zonkeymwi_notify(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a)
+{
+  struct subscription *watcher;
+  char user[LEN_MWI_USER];
+  char domain[LEN_MWI_DOMAIN];
+  int  msgnew = 0, msgold = 0;
+
+  switch (cmd) {
+  case CLI_INIT:
+    e->command = "zonkeymwi notify";
+    e->usage =
+      "Usage: zonkeymwi notify <user> <domain> <msgnew> <msgold>\n"
+      "       Notify MWI for subscription of <user> in <domain> with number of new messages <msgnum> and old messages <msgold>\n"
+      "       To clear MWI just set <msgnum> to 0\n"
+      "\n";
+    return NULL;
+  case CLI_GENERATE:
+    return NULL;
+  }
+  if(a->argc != 6) {
+    ast_cli(a->fd, "Wrong number of arguments.\n");
+    return CLI_SHOWUSAGE;
+  }
+  
+  ast_copy_string(user, a->argv[2], sizeof(user));
+  ast_copy_string(domain, a->argv[3], sizeof(domain));
+  msgnew = atoi(a->argv[4]);
+  msgold = atoi(a->argv[5]);
+
+  if((watcher=find_watcher(user, domain)) == NULL){
+    ast_cli(a->fd, "Subscription NOT found for %s@%s\n",user, domain);
+    return CLI_SUCCESS;
+  }
+  
+  if(send_notify(watcher, msgnew, msgold, a)){
+    ast_cli(a->fd, "Notify %s@%s with %d new messages and %d old messages was sent\n", user, domain, msgnew, msgold);
+  }
+
+  return CLI_SUCCESS;
+}
+
+/*!
+ * \brief Generate and send SIP NOTIFY request
+ * \param watcher struct
+ * \param number of waiting messages
+ * \param command arguments
+ * \return int
+ */
+int send_notify(struct subscription *watcher, int msgnew, int msgold, struct ast_cli_args *a)
+{
+  /*
+  ast_cli(a->fd, "MWI subscription details for %s@%s:\n", watcher->name, watcher->domain);
+  ast_cli(a->fd, "  To tag:   %s\n", watcher->to_tag);
+  ast_cli(a->fd, "  From tag: %s\n", watcher->from_tag);
+  ast_cli(a->fd, "  Call-ID:  %s\n", watcher->callid);
+  ast_cli(a->fd, "  Expires:  %d\n", watcher->expires);
+  ast_cli(a->fd, "  CSeq:     %d\n", watcher->cseq);
+  ast_cli(a->fd, "\n");
+  ast_cli(a->fd, "---- Waiting-Messages: %d\n", msgnum);
+  return 0;
+  */
+  struct ast_str *msg = ast_str_create(LEN_MWI_SIPMSG);
+  notify_create(msg, watcher, msgnew, msgold);
+  ast_cli(a->fd, "<------------------>\n%s\n<------------------>\n", ast_str_buffer(msg));
+  ast_free(msg);
+  return 1;
+}
+
+/*!
+ * \brief Create SIP NOTIFY message
+ * \param message string
+ * \param watcher struct
+ * \param new messages
+ * \param old messages
+ * \return void
+ */
+void notify_create(struct ast_str *msg, struct subscription *watcher, int new, int old)
+{
+  const char bindip[] = "pbx01.clusterpbx.ca";
+  const char contact[] = "asterisk";
+  // Date field
+  char date[256];
+  struct tm tm;
+  time_t t = time(NULL);
+  gmtime_r(&t, &tm);
+  strftime(date, sizeof(date), "%a, %d %b %Y %T GMT", &tm);
+
+  // message body
+  struct ast_str *body = ast_str_create(LEN_MWI_SIPMSG);
+  char vmexten[] = "*97";
+
+  ast_str_set(&body, 0, "Messages-Waiting: %s\r\n", new ? "yes": "no");
+  ast_str_append(&body, 0, "Message-Account: sip:%s@%s\r\n", vmexten, watcher->domain);
+  ast_str_append(&body, 0, "Voice-Message: %d/%d (%d/%d)\r\n", new, old, 0, 0);
+
+  // NOTIFY message constructing
+  ast_str_set(&msg, 0, "NOTIFY sip:%s@%s SIP/2.0\r\n", watcher->name, watcher->domain);
+  ast_str_append(&msg, 0, "Via: %s;branch=z9hG4bK%08lx\r\n", bindip, ast_random());
+  ast_str_append(&msg, 0, "To: sip:%s@%s;tag=%s\r\n", watcher->name, watcher->domain, watcher->to_tag);
+  ast_str_append(&msg, 0, "From: sip:%s@%s;tag=%s\r\n", watcher->name, watcher->domain, watcher->from_tag);
+  ast_str_append(&msg, 0, "Date: %s\r\n", date);
+  ast_str_append(&msg, 0, "Call-id: %s\r\n", watcher->callid);
+  ast_str_append(&msg, 0, "CSeq: %d NOTIFY\r\n", watcher->cseq + 1);
+  ast_str_append(&msg, 0, "Contact: <%s@%s>\r\n", contact, bindip);
+  ast_str_append(&msg, 0, "Event: message-summary\r\n");
+  ast_str_append(&msg, 0, "Subscription-State: active\r\n");
+  ast_str_append(&msg, 0, "Content-Type: application/simple-message-summary\r\n");
+  ast_str_append(&msg, 0, "Content-Length: %zd\r\n", ast_str_strlen(body));
+  ast_str_append(&msg, 0, "\r\n");
+  ast_str_append(&msg, 0, "%s", ast_str_buffer(body));
+
+  ast_free(body);
 }
 
 /*!
